9ca8f52cebaee6e19f3bbe7dcc74d16441ca16e9
shreyash2101 <chamaria.21.shreyash@gmail.com>
2024-04-27 16:16:22 +0530

    changed as per the golang backend

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -104,13 +104,13 @@ const JobPage = ({ deleteJob }) => {
                 <h3 className="text-xl">Contact Email:</h3>
 
                 <p className="my-2 bg-indigo-100 p-2 font-bold">
-                  {job.company.contactEmail}
+                  {job.company.contactemail}
                 </p>
 
                 <h3 className="text-xl">Contact Phone:</h3>
 
                 <p className="my-2 bg-indigo-100 p-2 font-bold">
-                  {job.company.contactPhone}
+                  {job.company.contactphone}
                 </p>
               </div>
 
@@ -118,13 +118,13 @@ const JobPage = ({ deleteJob }) => {
               <div className="bg-white p-6 rounded-lg shadow-md mt-6">
                 <h3 className="text-xl font-bold mb-6">Manage Job</h3>
                 <Link
-                  to={`/edit-job/${job.id}`}
+                  to={`/edit-job/${job._id}`}
                   className="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Edit Job
                 </Link>
                 <button
-                  onClick={() => onDeleteClick(job.id)}
+                  onClick={() => onDeleteClick(job._id)}
                   className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >
                   Delete Job
